20a3e3f5d5f5d5917c3d3e7747a837f3ac7155f3
Isaac Chapman <isaac@isaacchapman.com>
2015-11-05 16:17:40 -0800

    Reactivating deploy

--- scripts/solano-docker_build.sh
+++ scripts/solano-docker_build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# This file is no longer in use, but is left for historical reasons.
-# See solano.yml and ./scripts/solano-docker_build.sh
-exit 0
+# Build and start docker container
 
+set -o errexit -o pipefail # Exit on error
+
+# Ensure docker login credentials are set
 if [[ -z "$DOCKER_EMAIL" || -z "$DOCKER_USER" || -z "$DOCKER_PASSWORD" ]]; then
   echo 'ERROR: The $DOCKER_EMAIL, $DOCKER_USER, and $DOCKER_PASSWORD values are not all set!'
   echo 'Please use `solano config:add repo <key> <value>` to set these values'
@@ -10,7 +11,14 @@ if [[ -z "$DOCKER_EMAIL" || -z "$DOCKER_USER" || -z "$DOCKER_PASSWORD" ]]; then
   exit 1
 fi
 
+# Build docker image
 sudo docker login -e=$DOCKER_EMAIL -u=$DOCKER_USER -p=$DOCKER_PASSWORD # Not necessary for public docker repostiories 
 sudo docker pull ubuntu:14.04
 sudo docker build -t $DOCKER_USER/$DOCKER_APP:$TDDIUM_SESSION_ID .
+
+# Start docker container and record ID and IP address
+CID=$(sudo docker run -d --expose=80 $DOCKER_USER/$DOCKER_APP:$TDDIUM_SESSION_ID)
+echo $CID > $TDDIUM_REPO_ROOT/container-$TDDIUM_SESSION_ID.cid
+IP_ADDR=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' $CID)
+echo $IP_ADDR > $TDDIUM_REPO_ROOT/container-$TDDIUM_SESSION_ID.ip
 

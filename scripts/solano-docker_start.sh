9927ec609b9d0522ffb300dfa8de339dca0361f5
Matthew Albani <Matthew.jalbani@gmail.com>
2016-10-10 13:54:01 -0700

    Update solano-docker_start.sh

--- scripts/solano-docker_start.sh
+++ scripts/solano-docker_start.sh
@@ -7,3 +7,4 @@ CID=$(sudo docker run -d --expose=80 $DOCKER_USER/$DOCKER_APP:$TDDIUM_SESSION_ID
 echo $CID > $TDDIUM_REPO_ROOT/container-$TDDIUM_SESSION_ID.cid
 IP_ADDR=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' $CID)
 echo $IP_ADDR > $TDDIUM_REPO_ROOT/container-$TDDIUM_SESSION_ID.ip
+cat $TDDIUM_REPO_ROOT/container-$TDDIUM_SESSION_ID.ip
